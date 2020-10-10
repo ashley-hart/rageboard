f7e5271828416f7b2d3e5bb9b4ec7088e1249dcd
Walid Islam <wwalidislam@gmail.com>
2020-10-09 23:56:45 -0400

    added calcCPS, need to get time

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
+var CPS = 0, numChar = 0;
+var charNumArray = []; // This array will hold 
+ charArray = [];
+
 $(document).ready(function(){
 
-// all jquery functions must go here. this is the equivalent of the main function
+    $(document).on("keypress", function(){
+        if (CPS == 0){
+            numChar++;
+            startTimer(); // temporary variable name for Kensal's time function
+        }
+        else{
+            CPS = calcCPS();
+            
+        }
+    })
 
 })
+
+// Functions
+
+function calcCPS(){
+
+    let timeElapsed = getTimeElapsed(); //this function gets the amount of time passed since startTimer has been called
+    numChar++;
+
+    return numChar / timeElapsed;
+
+}
