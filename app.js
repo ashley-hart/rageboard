35d6ded647e50146bacf18f532c98946f4177367
Kensal Ramos <kensalramos@gmail.com>
2020-10-10 01:02:36 -0400

    Add 2-D Array to app.js
    This 2-D array has 101 indexes for every possible input in a keyboard. In the subarrays, the first index is the character and the second index the number of times this character has been pressed.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var CPS = 0, numChar = 0, flag = 0;
-var charNumArray = []; // This array will hold 
-var charArray = [];
+var charArray; // Holds characters and number of times pressed
+var startTime, endTime, numChar = -1;
 
 $(document).ready(function(){
 
@@ -8,6 +8,7 @@ $(document).ready(function(){
         if (flag == 0){
             numChar++;
             flag = 1;
+            create2DArray();
             start(); // temporary variable name for Kensal's time function
         }
         else {
@@ -17,15 +18,25 @@ $(document).ready(function(){
 
 });
 
-var startTime, endTime;
-
 // Functions
+
+// Create a n-dimensional 
+function create2DArray() {
+
+  charArray = new Array(101);
+
+  // Initialize every index 
+  for (var i = 0; i < charArray.length; i++) {
+    charArray[i] = new Array(2);
+    charArray[i][1] = 0; // Initialize all numOfTimes to 0
+  }
+
+}
 
 function calcCPS() {
 
     let timeElapsed = getTimeElapsed(); //this function gets the amount of time passed since startTimer has been called
     numChar++;
-
     alert("CPS = " + (numChar / timeElapsed));
     return numChar / timeElapsed;
 
