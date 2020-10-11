88914800ec0a86ddd858e4c2a117f222492a756f
Ashley Hart <ashleyhart5618@gmail.com>
2020-10-11 07:24:21 -0400

    keyboard shaking wip

--- keyboard.js
+++ keyboard.js
@@ -21,10 +21,11 @@ const Keyboard = {
 
         // Create main elements
         this.elements.main = document.createElement("div");
-        this.elements.keysContainer = document.createElement("div");
+        // this.elements.keysContainer = document.createElement("div");
+        this.elements.keysContainer = document.getElementById("kb-container");
 
-        // console.log("Appending kb to: ");
-        // containerDiv.appendChild(this.elements.keysContainer);  
+        console.log("Appending kb to: ");
+        console.log(this.elements.keysContainer);  
 
         // Setup main elements
         this.elements.main.classList.add("keyboard");
